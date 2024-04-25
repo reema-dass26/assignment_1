500170f0810183cdc942c18f587016c5d4ee0e8c
reema-dass26 <reemaworkmail@gmail.com>
2024-04-25 14:01:48 +0200

    class_models.py file updated

--- dlvc/models/class_model.py
+++ dlvc/models/class_model.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from pathlib import Path
+from torchvision.models import resnet18 as model
 
 class DeepClassifier(nn.Module):
     def __init__(self, net: nn.Module):
@@ -15,15 +16,13 @@ class DeepClassifier(nn.Module):
         '''
         Saves the model, adds suffix to filename if given
         '''
+        torch.save(model, save_dir)
 
-        ## TODO implement
-        pass
 
     def load(self, path):
         '''
         Loads model from path
         Does not work with transfer model
         '''
-        
-        ## TODO implement
-        pass
+        model = torch.load(path)
+        model.eval()
