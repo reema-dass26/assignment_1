8fcc5b8d548ed772389f398b2194f9d6fdb5ad71
Daniel Melichar <daniel@melichar.xyz>
2024-04-25 13:00:36 +0200

    CIFAR10 Dataset

--- dlvc/datasets/cifar10.py
+++ dlvc/datasets/cifar10.py
@@ -1,6 +1,7 @@
 import pickle
 from typing import Tuple
 import numpy as np
+from pathlib import Path
 
 
 from dlvc.datasets.dataset import  Subset, ClassificationDataset
@@ -24,20 +25,68 @@ class CIFAR10Dataset(ClassificationDataset):
         Images are loaded in the order they appear in the data files
         and returned as uint8 numpy arrays with shape (32, 32, 3), in RGB channel order.
         '''
+        p = Path(fdir)
+
+        data = sorted(list(p.glob("data_*")))
+        test = list(p.glob("test_*"))
+
+        if not p.exists() or len(data)+len(test) != 6: 
+            raise ValueError
+
+
+        training, validation = data[:-1], data[-1]
+
+
+        training_dict = self._unpickle(training[0])
+        training_data = training_dict[b'data']
+        training_labels = training_dict[b'labels']
+
+        for d in training[1:]:
+            training_dict = self._unpickle(d)
+            training_data = np.vstack((training_data, training_dict[b'data']))
+            training_labels += training_dict[b'labels']
+
+        training_data = training_data.reshape((len(training_data), 3, 32, 32))
+        training_data = np.rollaxis(training_data, 1, 4)
+        training_labels = np.array(training_labels)
+
+        validation_dict = self._unpickle(validation)
+        validation_data = validation_dict[b'data']
+        validation_labels = validation_dict[b'labels']
+        validation_data = validation_data.reshape(len(validation_data), 3, 32, 32)
+        validation_data = np.rollaxis(validation_data, 1, 4)
+        validation_labels = np.array(validation_labels)
+
+        test_dict = self._unpickle(test[0])
+        test_data = test_dict[b'data']
+        test_labels = test_dict[b'labels']
+        test_data = test_data.reshape(len(test_data), 3, 32, 32)
+        test_data = np.rollaxis(test_data, 1, 4)
+        test_labels = np.array(test_labels)
+
+        if subset == Subset.TRAINING:
+            self.data = training_data
+            self.labels = training_labels
+
+        if subset == Subset.TEST:
+            self.data = test_data
+            self.labels = test_labels
+
+        if subset == Subset.VALIDATION:
+            self.data = validation_data
+            self.labels = validation_labels
 
         self.classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
         
-        ## TODO implement
-        # See the CIFAR-10 website on how to load the data files
-        pass
+        self.transform = transform    
 
     def __len__(self) -> int:
         '''
         Returns the number of samples in the dataset.
         '''
-        ## TODO implement
-        pass
+        
+        return len(self.data)
 
     def __getitem__(self, idx: int) -> Tuple:
         '''
@@ -46,13 +95,25 @@ class CIFAR10Dataset(ClassificationDataset):
         Applies transforms if not None.
         Raises IndexError if the index is out of bounds.
         '''
-        ## TODO implement
-        pass
+        sample = self.data[idx]
+        label = self.labels[idx]
+                
+        if self.transform:
+            sample = self.transform(sample)
+            
+        return sample, label
+
 
     def num_classes(self) -> int:
         '''
         Returns the number of classes.
         '''
-        ## TODO implement
-        pass
+        return len(self.classes)
 
+
+
+    def _unpickle(self, file):
+        """load the cifar-10 data"""
+        with open(file, 'rb') as fo:
+            data = pickle.load(fo, encoding='bytes')
+        return data
